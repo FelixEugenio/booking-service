6daa95c903ffa4e1b3dcf917038bb6bce0a101c2
Felix Eugenio Mavila <felixmacarenco@gmail.com>
2025-07-09 19:07:37 +0100

    test: test HomeControllerHandler method

---
+++ src/main/java/com/felix/booking/service/controller/HomeController.java
@@ -0,0 +1,13 @@
+package com.felix.booking.service.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping
+    public String HomeControllerHandler(){
+        return "Booking Service for salon microservice booking system ";
+    }
+}
